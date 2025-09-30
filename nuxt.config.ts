732a4e39968709e6855741182b1479d0762bf03d
Roberto Carlos <imagchine27@gmail.com>
2025-09-30 13:29:01 +0200

    Updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,3 @@
 export default defineNuxtConfig({
-    components: [
-        // Auto-import components based only on its name, not path,
-        {
-            path: "@/components",
-            pathPrefix: false,
-        },
-    ],
+    
 })
